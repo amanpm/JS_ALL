26f657eb8080801313a733acfd49c9b3cd6772af
amanpm <80583218+amanpm@users.noreply.github.com>
2024-12-11 10:56:08 +0000

    change

---
+++ 01_basic_js/comparision.js
@@ -0,0 +1,24 @@
+// We already know 
+
+console.log(null>0);  // false
+
+console.log(null==0);  //false
+
+console.log(null>=0);  //true
+
+console.log(null===null); //true
+
+
+
+// some more 
+
+console.log(0 == false)     // true
+console.log(0 === false )   // false, because they have different types (int, bool)
+console.log(1 == "1" )      // true
+console.log(1 === "1"  )      // false, because they have different types (int, string)
+console.log(null == undefined)   // true
+console.log(null === undefined) // false
+
+
+
+// you know that javascript is statically type or dynamically type 
---
+++ 01_basic_js/conversion_opreation.js
@@ -0,0 +1,93 @@
+//string to number 
+
+let number = "33abc";
+// console.log(typeof(number));
+
+let valueInNumber = Number(number);
+// console.log(typeof (valueInNumber));
+
+// console.log(valueInNumber);
+
+
+//string 
+
+let num = "33";
+// console.log(typeof(num));
+
+let valueINumber = Number(num);
+// console.log(typeof (valueINumber));
+
+// console.log(valueINumber);
+
+
+
+
+//string to number 
+
+let numb = null;
+// console.log(typeof(numb));
+
+let valueNumber = Number(numb);
+// console.log(typeof (valueNumber));
+
+// console.log(valueNumber);
+
+//
+
+let nu = undefined;
+// console.log(typeof(nu));
+
+let value = Number(nu);
+// console.log(typeof (value));
+
+// console.log(value);
+
+//
+let n = true;
+let nf = false;
+console.log(typeof(n));  // boolean
+console.log(nf);         // boolean
+
+let valueTrue = Number(n);
+console.log(typeof (valueTrue)); // number
+let valueFlase = Number(nf);
+console.log(typeof (valueFlase));   // number
+
+console.log(valueTrue); // 1
+console.log(valueFlase); // 0
+
+
+// visit again for string to number
+
+
+
+
+//visit again for boolean to number
+
+
+
+//visit again for boolean for string 
+
+
+
+// visit for number to boolean 
+
+
+
+// visit try many more 
+
+
+
+// Basic opreation 
+
+console.log(2*2); // 4
+
+console.log(2**3); // 8
+
+console.log(2%3); // 2
+
+// imp 
+
+console.log("1"+2+2);  // 122
+
+console.log(1+2+"2");  // 32
